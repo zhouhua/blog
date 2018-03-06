4f25fb62a566e4d721e4d0431df9b5fc4ab738c9
zhouhua <zhou--hua@163.com>
2018-03-06 23:04:38 +0800

    try gitalk

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -37,10 +37,10 @@ class PostTemplateDetails extends React.Component {
                 <script src="https://unpkg.com/gitalk@latest/dist/gitalk.min.js" />
                 <script dangerouslySetInnerHTML={{
                     __html: `var gitalk = new Gitalk({
-                        clientID: 'c6feba9cda7803e3c181',
-                        clientSecret: 'aed071632e47d501bc5c50a8a1540188a2db957d',
-                        repo: 'blog',
-                        owner: 'zhouhua-js',
+                        clientID: 'aa19479305bd5ae2cc05',
+                        clientSecret: '5514529cafdd958f72e2119eec0ff0fa876963ab',
+                        repo: 'blog-common',
+                        owner: 'zhouhua',
                         admin: ['zhouhua'],
                         id: window.location.pathname, // Ensure uniqueness and length less than 50
                         distractionFreeMode: false // Facebook-like distraction free mode
